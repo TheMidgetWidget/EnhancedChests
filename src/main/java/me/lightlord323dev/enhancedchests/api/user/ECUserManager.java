688cbbabcd49b04cb203dc8b9000415785fbd118
TheMidgetWidget <opklqth234>
2020-07-15 20:14:25 +0300

    Add user caching and start with command

--- src/main/java/me/lightlord323dev/enhancedchests/api/user/ECUserManager.java
+++ src/main/java/me/lightlord323dev/enhancedchests/api/user/ECUserManager.java
@@ -1,6 +1,11 @@
 package me.lightlord323dev.enhancedchests.api.user;
 
 import me.lightlord323dev.enhancedchests.api.manager.Manager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +14,7 @@ import java.util.UUID;
 /**
  * Created by Luda on 7/15/2020.
  */
-public class ECUserManager implements Manager {
+public class ECUserManager implements Manager, Listener {
 
     private List<ECUser> users;
 
@@ -23,13 +28,61 @@ public class ECUserManager implements Manager {
         // TODO SAVE CACHED USERS
     }
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        // TODO check for saved data, will create user objects for now
+        registerUser(new ECUser(e.getPlayer().getUniqueId()));
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e) {
+        // TODO save player data
+        unregisterUser(e.getPlayer());
+    }
+
     /**
-     *
      * @param uuid player uuid
      * @return ECUser object if found, null if not found
      */
     public ECUser getUser(UUID uuid) {
         return users.stream().filter(ecUser -> ecUser.getPlayerUUID().equals(uuid)).findAny().orElse(null);
+    }
+
+    /**
+     * caches user in manager
+     * @param user
+     */
+    private void registerUser(ECUser user) {
+        users.add(user);
+    }
+
+    /**
+     * de-caches user info
+     * @param user
+     */
+    private void unregisterUser(ECUser user) {
+        if (users.contains(user))
+            users.remove(user);
+    }
+
+    /**
+     * de-caches user info
+     * @param player
+     */
+    private void unregisterUser(Player player) {
+        ECUser user = getUser(player.getUniqueId());
+        if (user != null)
+            users.remove(user);
+    }
+
+    /**
+     * de-caches user info
+     * @param uuid
+     */
+    private void unregisterUser(UUID uuid) {
+        ECUser user = getUser(uuid);
+        if (user != null)
+            users.remove(user);
     }
 
 }
---
+++ src/main/java/me/lightlord323dev/enhancedchests/command/ECCommand.java
@@ -0,0 +1,17 @@
+package me.lightlord323dev.enhancedchests.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Created by Luda on 7/15/2020.
+ */
+public class ECCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
+
+        return true;
+    }
+}
